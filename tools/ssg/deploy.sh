907e3add3b9be52ac772c178072a8a2160ca9d81
yuinore <yuinore@hotmail.co.jp>
2024-07-04 21:57:15 +0900

    update script

--- tools/ssg/deploy.sh
+++ tools/ssg/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eux
 
-# ./tools/ssg/generate.sh
+echo "Please run ./tools/ssg/generate.sh manually."
 
 # TODO: CNAME
-# cp CNAME dist/
+cp CNAME dist/
 
 # できれば Ruby の gem で統一したかったけど一旦 npm で
 npx gh-pages -d dist
--- tools/ssg/generate.sh
+++ tools/ssg/generate.sh
@@ -9,10 +9,13 @@ SERVER_URL="http://wiki.mid2bms.net/"
 rm -rf dist/
 # wget -r -l inf -k -nc -nH -p -E -e robots=off -P ./dist $SERVER_URL
 # wget -r -l inf -w 1 -k -nc -nH -p -E -e robots=off --reject-regex "(.*)\?(.*)" -P ./dist $SERVER_URL
-wget -r -l inf -w 1 -k -nc -nH -p -E -e robots=off --accept-regex "^([^\?]+|.*cmd=list.*)$" -P ./dist $SERVER_URL
+wget -r -l inf -w 1 -k -nc -nH -p -E -e robots=off --accept-regex "^([^\?]+|.*cmd=list.*|.*\.css.*|.*\.png|.*\.jpg)$" -P ./dist $SERVER_URL
 
 # # ページ上部へのリンクが FrontPage.html へのリンクになってしまうので対症療法
 find dist/ -name '*.html' -exec sed -E -i 's!(\.\./)*FrontPage\.html!/!g' {} \;
+find dist/ -name '*.html' -exec sed -E -i 's!\.html"!"!g' {} \;
+# find dist/ -name '*.html' -exec sed -E -i 's!\.html%3F!%3F!g' {} \;
+find dist/ -name '*.html' -exec sed -E -i 's!\.html#!#!g' {} \;
 
 # kill -15 $!
 
